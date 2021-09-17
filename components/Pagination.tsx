5e01a6a1b4d2cc877d3f23507790b09cb83de2c0
Mitchell Victoriano <mitchell.victoriano@gmail.com>
2021-09-17 14:11:03 -0700

    fixed build issues

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,119 +1,129 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import useWindowSize from "../hooks/useWindowSize";
-const Pages = ({ currentPage, maxPage, query, pathname, ahead }) => {
-	const pageList = [];
-	const start = currentPage - ahead;
-	const end = currentPage + ahead;
-	for (let i = start; i <= end; i++) {
-		if (i >= 1 && i <= end && i <= maxPage) {
-			pageList.push(i);
-		}
-	}
 
-	return (
-		<>
-			{pageList.map((page) => (
-				<Link
-					key={page}
-					href={{
-						pathname,
-						query: { ...query, page }
-					}}
-				>
-					<a
-						className={
-							currentPage === page ? "page current" : "page"
-						}
-					>
-						{page}
-					</a>
-				</Link>
-			))}
-		</>
-	);
+interface PropTypes {
+  currentPage: number;
+  maxPage: number;
+  query: any;
+  pathname: string;
+  ahead: number;
+}
+const Pages = ({ currentPage, maxPage, query, pathname, ahead }: PropTypes) => {
+  const pageList = [];
+  const start = currentPage - ahead;
+  const end = currentPage + ahead;
+  for (let i = start; i <= end; i++) {
+    if (i >= 1 && i <= end && i <= maxPage) {
+      pageList.push(i);
+    }
+  }
+
+  return (
+    <>
+      {pageList.map((page) => (
+        <Link
+          key={page}
+          href={{
+            pathname,
+            query: { ...query, page },
+          }}
+        >
+          <a className={currentPage === page ? "page current" : "page"}>
+            {page}
+          </a>
+        </Link>
+      ))}
+    </>
+  );
 };
 
-const Pagination = ({ currentPage, maxPage }) => {
-	const router = useRouter();
-	const pathname = router.pathname;
-	const query = router.query;
+const Pagination = ({
+  currentPage,
+  maxPage,
+}: {
+  currentPage: number;
+  maxPage: number;
+}) => {
+  const router = useRouter();
+  const pathname = router.pathname;
+  const query = router.query;
 
-	const [width, height] = useWindowSize();
-	const isMobile = width < 768 ? true : false;
-	const ahead = isMobile ? 2 : 5;
-	return (
-		<>
-			{maxPage > 1 && (
-				<section className="pagination">
-					{currentPage > ahead + 1 && (
-						<Link
-							href={{
-								pathname,
-								query: { ...query, page: 1 }
-							}}
-						>
-							<a className="first">
-								<i className="fa fa-chevron-left"></i>
-								<i className="fa fa-chevron-left"></i>
-							</a>
-						</Link>
-					)}
-					{currentPage !== 1 && (
-						<>
-							<Link
-								href={{
-									pathname,
-									query: { ...query, page: currentPage - 1 }
-								}}
-							>
-								<a className="previous">
-									<i className="fa fa-chevron-left"></i>
-								</a>
-							</Link>
-						</>
-					)}
+  const [width, height] = useWindowSize();
+  const isMobile = width < 768 ? true : false;
+  const ahead = isMobile ? 2 : 5;
+  return (
+    <>
+      {maxPage > 1 && (
+        <section className="pagination">
+          {currentPage > ahead + 1 && (
+            <Link
+              href={{
+                pathname,
+                query: { ...query, page: 1 },
+              }}
+            >
+              <a className="first">
+                <i className="fa fa-chevron-left"></i>
+                <i className="fa fa-chevron-left"></i>
+              </a>
+            </Link>
+          )}
+          {currentPage !== 1 && (
+            <>
+              <Link
+                href={{
+                  pathname,
+                  query: { ...query, page: currentPage - 1 },
+                }}
+              >
+                <a className="previous">
+                  <i className="fa fa-chevron-left"></i>
+                </a>
+              </Link>
+            </>
+          )}
 
-					<Pages
-						currentPage={currentPage}
-						maxPage={maxPage}
-						pathname={pathname}
-						query={query}
-						ahead={ahead}
-					/>
-					{currentPage !== maxPage && (
-						<>
-							<Link
-								href={{
-									pathname,
-									query: {
-										...query,
-										page: currentPage + 1
-									}
-								}}
-							>
-								<a className="next">
-									<i className="fa fa-chevron-right"></i>
-								</a>
-							</Link>
-							{maxPage >= currentPage + ahead + 1 && (
-								<Link
-									href={{
-										pathname,
-										query: { ...query, page: maxPage }
-									}}
-								>
-									<a className="last">
-										<i className="fa fa-chevron-right"></i>
-										<i className="fa fa-chevron-right"></i>
-									</a>
-								</Link>
-							)}
-						</>
-					)}
-				</section>
-			)}
-		</>
-	);
+          <Pages
+            currentPage={currentPage}
+            maxPage={maxPage}
+            pathname={pathname}
+            query={query}
+            ahead={ahead}
+          />
+          {currentPage !== maxPage && (
+            <>
+              <Link
+                href={{
+                  pathname,
+                  query: {
+                    ...query,
+                    page: currentPage + 1,
+                  },
+                }}
+              >
+                <a className="next">
+                  <i className="fa fa-chevron-right"></i>
+                </a>
+              </Link>
+              {maxPage >= currentPage + ahead + 1 && (
+                <Link
+                  href={{
+                    pathname,
+                    query: { ...query, page: maxPage },
+                  }}
+                >
+                  <a className="last">
+                    <i className="fa fa-chevron-right"></i>
+                    <i className="fa fa-chevron-right"></i>
+                  </a>
+                </Link>
+              )}
+            </>
+          )}
+        </section>
+      )}
+    </>
+  );
 };
 export default Pagination;
--- hooks/useFetchMovies.tsx
+++ hooks/useFetchMovies.tsx
@@ -3,45 +3,48 @@ import { useRouter } from "next/router";
 import axios from "axios";
 
 const useFetchMovies = (location, setIsLoading) => {
-	const router = useRouter();
-	const [response, setResponse] = useState({
-		movies: [],
-		page: 1,
-		maxPage: 1
-	});
-	useEffect(async () => {
-		setIsLoading(true);
-		if (router.isReady) {
-			try {
-				const { slug, ...query } = router.query;
-				console.log(query);
-				let URI = "";
-				switch (location) {
-					case "user":
-						URI = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/user/${slug}`;
-						break;
-					case "movie":
-						URI = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/movie/${slug}`;
-						break;
-					case "personal":
-						URI = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/user/${slug}/personal`;
-						break;
+  const router = useRouter();
+  const [response, setResponse] = useState({
+    movies: [],
+    page: 1,
+    maxPage: 1,
+  });
+  const fetchAPI = async (URI, params) => {
+    const res = await axios.get(URI, params);
+    const data = res.data;
+    setResponse(data);
+    setIsLoading(false);
+    return data;
+  };
+  useEffect(() => {
+    setIsLoading(true);
+    if (router.isReady) {
+      try {
+        const { slug, ...query } = router.query;
+        console.log(query);
+        let URI = "";
+        switch (location) {
+          case "user":
+            URI = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/user/${slug}`;
+            break;
+          case "movie":
+            URI = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/movie/${slug}`;
+            break;
+          case "personal":
+            URI = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/user/${slug}/personal`;
+            break;
 
-					default:
-						break;
-				}
-				const params = { params: query, timeout: 1000 * 60 * 5 };
-				const res = await axios.get(URI, params);
-				const data = res.data;
-				console.log(data);
-				setResponse(data);
-				setIsLoading(false);
-			} catch (err) {
-				console.error(err);
-			}
-		}
-	}, [router]);
-	return response;
+          default:
+            break;
+        }
+        const params = { params: query, timeout: 1000 * 60 * 5 };
+        fetchAPI(URI, params);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }, [router]);
+  return response;
 };
 
 export default useFetchMovies;
